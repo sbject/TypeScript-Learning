d4576d4a214be08e9bd04e9ee27b718de867483a
Sergey Bogdanov <sbject@gmail.com>
2021-05-24 11:03:39 -0400

    finalized characters collection and it can now be part of sortable interface

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,12 @@
 import {Sorter} from './sorter';
 import {NumbersCollection} from './NumbersCollection';
+import {CharactesCollection} from './CharactersCollection'; 
 
 const numbersCollection = new NumbersCollection([10,3,-5,0]);
+const charsCollection = new CharactesCollection('hello');
 const sorter = new Sorter(numbersCollection);
+const sorter2 = new Sorter(charsCollection);
 sorter.sort();
+sorter2.sort();
+console.log(charsCollection.data);
 console.log(numbersCollection.data);
