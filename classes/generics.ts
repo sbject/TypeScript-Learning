9f26594bcbc5ca748d38a2ed7a04cd9ec18cc9cd
Sergey Bogdanov <sbject@gmail.com>
2021-05-30 21:47:19 -0400

    added generic example to a classes folder

---
+++ classes/generics.ts
@@ -0,0 +1,48 @@
+class ArrayOfNumbers {
+    constructor(public colllection:number[]) {}
+
+    get(index:number):number {
+        return this.colllection[index];
+    }
+}
+
+class ArrayOfStrings {
+    constructor(public collection:string[]){}
+
+    get(index:number):string {
+        return this.collection[index];
+    }
+}
+
+class ArrayOfAnything<T> {
+    constructor(public collection:T[]){}
+
+    get(index:number):T {
+        return this.collection[index];
+    }
+}
+
+const arr = new ArrayOfAnything(['a','b']);
+
+// Example of generics with functions
+
+function printStrings(arr:string[]):void{
+    for(let i=0;i<arr.length;i++ ){
+        console.log(arr[i]);
+    }
+}
+
+function printNumbers(arr:number[]):void{
+    for(let i=0;i<arr.length;i++ ){
+        console.log(arr[i]);
+    }
+}
+
+function printAnything<T>(arr:T[]):void{
+    for(let i=0;i<arr.length;i++ ){
+        console.log(arr[i]);
+    }
+}
+
+printAnything<string>(['a','b','c']);
+printAnything(['a','b','c']);
