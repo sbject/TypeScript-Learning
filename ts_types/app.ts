0b6795ac892ee06140a68265af11ccb0158d1096
Sergey Bogdanov <sbject@gmail.com>
2021-06-01 08:39:26 -0400

    added tuple to app.ts in types example

--- ts_types/app.ts
+++ ts_types/app.ts
@@ -1,7 +1,13 @@
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Sergey",
   age: 36,
   hobbies: ["hockey", "photo", "soccer"],
+  role: [34, "center"],
 };
 
 for (const h of person.hobbies) {
